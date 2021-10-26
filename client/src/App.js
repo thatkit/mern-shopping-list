d4df2a913581fd6f19aa04721db6e88a168d3883
Nikita <emper137137@gmail.com>
2021-10-27 09:08:45 +1000

    navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+//components
+import NavbarComp from './components/NavbarComp/NavbarComp'
+
 const App = () => {
   return (
     <div className="App">
+      <NavbarComp />
       <h1>Hello</h1>
     </div>
   );
---
+++ client/src/components/NavbarComp/NavbarComp.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from 'reactstrap';
+
+const NavbarComp = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar 
+      color="dark" 
+      dark expand="sm" 
+      className="mb-5"
+    >
+      <Container>
+        <NavbarBrand href="/">Shopping List</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto float-right" navbar>
+            <NavItem>
+              <NavLink href="https://github.com/thatkit">
+                GitHub
+              </NavLink>
+            </NavItem>
+          </Nav>
+
+        </Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarComp;
