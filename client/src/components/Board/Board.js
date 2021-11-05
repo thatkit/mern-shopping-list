adcc56cd0620a087308f6dab7858687819a1e2e0
Nikita <emper137137@gmail.com>
2021-11-06 09:13:53 +1000

    promise rejected state UI message addition

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -1,14 +1,17 @@
 import ShoppingList from './ShoppingList/ShoppingList';
 import ItemModal from './ItemModal/ItemModal';
 import LoadingSpinner from './LoadingSpinner/LoadingSpinner';
+import ErrorMessage from './ErrorMessage/ErrorMessage';
 
 import { useSelector } from 'react-redux';
 
 const Board = () => {
     const isLoading = useSelector(state => state.shoppingList.isLoading);
+    const hasError = useSelector(state => state.shoppingList.hasError);
     return (
         <>
             {isLoading && <LoadingSpinner />}
+            {hasError && <ErrorMessage />}
             <ShoppingList />
             <ItemModal />
         </>
