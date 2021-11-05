ee5f2ddb61d32556c1044972a64df0c0208a0081
Nikita <emper137137@gmail.com>
2021-11-05 19:31:32 +1000

    list order BUG FIX

--- routes/api/items.js
+++ routes/api/items.js
@@ -11,7 +11,7 @@ const Item = require('../../models/Item');
 router.get('/', (req, res) => {
     Item
         .find()
-        .sort({ date: -1 })
+        .sort({ date: 1 })
         .then(items => res.json(items))
 });
 
