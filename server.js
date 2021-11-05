a3265bcc5fe7f302b59a5c322b42814d4434aae0
Nikita <emper137137@gmail.com>
2021-11-05 11:35:30 +1000

    deploy 1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 require('dotenv').config({ path: './config/.env' });
 
 const items = require('./routes/api/items');
@@ -19,6 +20,15 @@ mongoose
 
 // Use Routes
 app.use('/api/items', items);
+
+// Serve static assets in PRODUCTION
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
