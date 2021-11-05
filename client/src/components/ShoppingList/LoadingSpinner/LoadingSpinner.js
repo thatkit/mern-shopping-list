0873d2072442b8ea7b41d62b3cc21a5e1df2218c
Nikita <emper137137@gmail.com>
2021-11-05 19:19:54 +1000

    spinner margin

--- client/src/components/ShoppingList/LoadingSpinner/LoadingSpinner.js
+++ client/src/components/ShoppingList/LoadingSpinner/LoadingSpinner.js
@@ -1,7 +1,7 @@
 import { Spinner } from 'reactstrap';
 
 const LoadingSpinner = () => {
-    return <Spinner color="dark"> </Spinner>
+    return <Spinner color="dark" className="mt-2 mb-2"> </Spinner>
 }
 
 export default LoadingSpinner;
